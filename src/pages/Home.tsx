d8f24f4b8b7f864f5657a9534789f3bfa7532834
Amhjadkhan <amjadpogramer@gmail.com>
2024-09-12 00:16:13 +0400

    product and store cartslice commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Carousel from "../components/ui/Carousel"
+import Product from "./product/Product"
 
 const Home = () => {
   return (
     <div>
       <Carousel />
+      <Product />
     </div>
 
   )
--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -1,8 +1,20 @@
+import { useGetProductsQuery } from "../../redux/api/api";
 
-const product = () => {
+const Product = () => {
+  const { data: products} = useGetProductsQuery({});
+  console.log("product: ", products?.data?.result);
   return (
-    <div>product</div>
-  )
-}
+    <div className="container flex flex-col mx-auto p-6 lg:flex-row lg:mb-0">
+      <div className="container mx-auto mt-16 px-6">
+        <h2 className="mb-6 text-4xl font-semibold text-center uppercase">
+          Products page
+        </h2>
+        <p className="max-w-md mx-auto text-center text-grayishBlue">
+         hlw product
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default product
+export default Product;
