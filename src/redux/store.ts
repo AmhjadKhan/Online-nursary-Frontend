9018d912f932d221f174ae592df9971b51718dbe
Amhjadkhan <amjadpogramer@gmail.com>
2024-09-13 17:15:21 +0400

    stre code eeror commit

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { baseApi } from "./api/api";
+import persistReducer  from "redux-persist";
 
+
+const persistedCartReducer = persistReducer();
 
 export const store = configureStore({
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer,
+    cart: persistedCartReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(baseApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
+export const persistor = persistStore(store);
